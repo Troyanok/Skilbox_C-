c30a80f3579a2751497d166d490782472bf0a2b0
Troyanok <83295437+Troyanok@users.noreply.github.com>
2026-05-21 15:18:20 +0300

    Implement snake matrix initialization and printing
    Added functions to initialize and print a snake matrix.

--- task13/main6.cpp
+++ task13/main6.cpp
@@ -1,10 +1,40 @@
 #include <iostream>
-#include <string>
-#include <cctype>
-
 using namespace std;
+
+const int MATRIX_SIZE { 5 };
+
+void InitialSnakeMatrix(int matrix[MATRIX_SIZE][MATRIX_SIZE])
+{
+    int value { 0 };
+    for (int i = 0; i < MATRIX_SIZE; i++)
+    {
+        int changePos = 1 - 2 * (i % 2);
+        int startPos = (MATRIX_SIZE - 1) * (i % 2);
+        for (int j = 0; j < MATRIX_SIZE; j++)
+        {
+            int col = startPos + j * changePos;
+            matrix[i][col] = value++;
+        }
+    }
+}
+
+void PrintMatrix(const int matrix[MATRIX_SIZE][MATRIX_SIZE])
+{
+    for (int i = 0; i < MATRIX_SIZE; i++)
+    {
+        for (int j = 0; j < MATRIX_SIZE; j++)
+        {
+            cout << matrix[i][j] << "\t";
+        }
+        cout << endl;
+    }
+}
 
 int main()
 {
+    int matrix[MATRIX_SIZE][MATRIX_SIZE];
+    InitialSnakeMatrix(matrix);
+    PrintMatrix(matrix);
+    
     return 0;
 }
