cece5ae0bf9ee6d46965cef7798b52f940475a16
Troyanok <83295437+Troyanok@users.noreply.github.com>
2026-06-17 09:32:33 +0300

    Add reverse function to reverse an array

--- task15/main2.cpp
+++ task15/main2.cpp
@@ -4,7 +4,26 @@
 
 using namespace std;
 
+void reverse(int* arr, int length)
+{
+    for (int i = 0; i < length / 2; i++)
+    {
+        int temp = arr[i];
+        arr[i] = arr[length - 1 - i];
+        arr[length - 1 - i] = temp;
+    }
+}
+
 int main()
 {
+    int data[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int length = sizeof(data) / sizeof(int);
+    reverse(data, length);
+
+    for (int i = 0; i < length; i++)
+    {
+        cout << data[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
