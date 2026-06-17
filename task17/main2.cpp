f2796846a37f7b796652d58c01149bf05e7df291
Troyanok <83295437+Troyanok@users.noreply.github.com>
2026-06-17 13:04:24 +0300

    Update main2.cpp

--- task17/main2.cpp
+++ task17/main2.cpp
@@ -17,13 +17,22 @@ int main()
         return -1;
     }
 
-    string line;
-    while (getline(file, line))
+    const size_t bufferSize = 4096;
+    char buffer[bufferSize];
+
+    while (file.read(buffer, bufferSize))
     {
-        cout << line << endl;
+        cout.write(buffer, bufferSize);
+    }
+
+    streamsize bytesRead = file.gcount();
+    if (bytesRead > 0)
+    {
+        cout.write(buffer, bytesRead);
     }
 
     file.close();
+    cout << endl;
 
     return 0;
 }
