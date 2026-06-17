2d8db7dbcd1881a8e0261a95124e0e4cd16b0478
Troyanok <83295437+Troyanok@users.noreply.github.com>
2026-06-17 10:28:35 +0300

    Update main2.cpp

--- task16/main2.cpp
+++ task16/main2.cpp
@@ -1,10 +1,49 @@
 #include <iostream>
 #include <string>
 #include <cctype>
+#include <vector>
 
 using namespace std;
 
+void CountRabbitJump(int n, int k, vector<int>&path, int& totalCount)
+{
+    if (n < 0)
+    {
+        return;
+    }
+    
+    if (n == 0)
+    {
+        cout << "(";
+        for (size_t i = 0; i < path.size(); i++)
+        {
+            cout << path[i];
+            if (i + 1 < path.size())
+            {
+                cout << ",";
+            }
+        }
+        cout << ")" << endl;
+        totalCount++;
+        return;
+    }
+    
+    for (int jump = 1; jump <= k; jump++)
+    {
+        path.push_back(jump);
+        CountRabbitJump(n - jump, k, path, totalCount);
+        path.pop_back();
+    }
+}
+
 int main()
 {
+    int total = 0;
+    int n = 3;
+    int k = 2;
+    vector<int> path = {};
+    CountRabbitJump(n, k, path, total);
+    
+    cout << "Всего способов: " << total << endl;
     return 0;
 }
