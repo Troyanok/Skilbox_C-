5f1cad9c87363d5e82604f002afbcb939a0ec120
Troyanok <83295437+Troyanok@users.noreply.github.com>
2026-06-17 12:54:43 +0300

    Update main2.cpp

--- task17/main2.cpp
+++ task17/main2.cpp
@@ -1,10 +1,29 @@
+#include <fstream>
 #include <iostream>
 #include <string>
-#include <cctype>
 
 using namespace std;
 
 int main()
 {
+    string filePath;
+    cout << "Введите путь к файлу: ";
+    cin >> filePath;
+
+    fstream file(filePath);
+    if (!file.is_open())
+    {
+        cout << "Failed to open file: " << filePath << endl;
+        return -1;
+    }
+
+    string line;
+    while (getline(file, line))
+    {
+        cout << line << endl;
+    }
+
+    file.close();
+
     return 0;
 }
