eb9194420bcfe1af698c839de45a480b881a7339
Troyanok <83295437+Troyanok@users.noreply.github.com>
2026-06-17 10:55:56 +0300

    Update main3.cpp

--- task16/main3.cpp
+++ task16/main3.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
 #include <string>
 #include <cctype>
+#include <vector>
 
 using namespace std;
 
+void evendigits(long long n, int& ans)
+{
+    if (n == 0)
+    {
+        ans = 0;
+        return;
+    }
+    if (n < 10)
+    {
+        ans = (n % 2 == 0) ? 1 : 0;
+        return;
+    }
+    evendigits(n / 10, ans);
+    if ((n % 10) % 2 == 0)
+    {
+        ans++;
+    }
+}
+
 int main()
 {
+    int ans = 0;
+    evendigits(9223372036854775806, ans);
+    
+    cout << "Количество чётных символов: " << ans << endl;
     return 0;
 }
