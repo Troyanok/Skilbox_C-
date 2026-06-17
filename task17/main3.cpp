255750bf8d72d05187adb9e5e15bb707f46c75ae
Troyanok <83295437+Troyanok@users.noreply.github.com>
2026-06-17 13:34:01 +0300

    Update main3.cpp

--- task17/main3.cpp
+++ task17/main3.cpp
@@ -1,10 +1,43 @@
+#include <fstream>
 #include <iostream>
 #include <string>
-#include <cctype>
 
 using namespace std;
 
 int main()
 {
+    ifstream file("data.txt");
+    if (!file.is_open())
+    {
+        cout << "Failed to open file!" << endl;
+        return -1;
+    }
+
+    string userName;
+    string userSername;
+    double userValue;
+    string userDate;
+
+    string maxValueUserName = "";
+    string maxValueUserSername = "";
+    double maxUserValue = 0;
+    double totalValue = 0;
+
+    while (file >> userName >> userSername >> userValue >> userDate)
+    {
+        totalValue += userValue;
+        if (userValue > maxUserValue)
+        {
+            maxValueUserName = userName;
+            maxValueUserSername = userSername;
+            maxUserValue = userValue;
+        }
+    }
+
+    file.close();
+
+    cout << "Общее количество выплаченных средств: " << totalValue << endl;
+    cout << "Человек с максимальной выплатой: " <<  maxValueUserName << " " << maxValueUserSername << " стоимость: " << maxUserValue << endl;
+
     return 0;
 }
