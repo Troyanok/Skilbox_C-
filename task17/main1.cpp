cf659a571c949f5a79557e0b681a4c057d93ea70
Troyanok <83295437+Troyanok@users.noreply.github.com>
2026-06-17 12:50:42 +0300

    Update main1.cpp

--- task17/main1.cpp
+++ task17/main1.cpp
@@ -1,10 +1,36 @@
+#include <fstream>
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-
 int main()
 {
-   return 0;
+    string subStr;
+    cout << "Введите подстроку для поиска: ";
+    cin >> subStr;
+
+    fstream file("words.txt");
+    if (!file.is_open())
+    {
+        cout << "Failed to open file!" << endl;
+        return -1;
+    }
+
+    string content;
+    int count = 0;
+
+    while(file >> content)
+    {
+        if (content == subStr)
+        {
+            count ++;
+        }
+    }
+
+    file.close();
+
+    cout << "Число вхождений подстроки " << subStr << " : " << count << endl;
+
+    return 0;
 }
