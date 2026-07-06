f0576f78cb3f4ea46296675eff9af00ac4a52220
Troyanok <83295437+Troyanok@users.noreply.github.com>
2026-07-06 16:35:27 +0300

    Update main2.cpp

--- task18/main2.cpp
+++ task18/main2.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
-#include <string>
-#include <cctype>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 int main()
 {
+    srand(static_cast<unsigned>(time(nullptr)));
+
+    int width, height;
+    cout << "Введите ширину картины: ";
+    cin >> width;
+    cout << "Введите высоту картины: ";
+    cin >> height;
+    if ((width <= 0) || (height <= 0))
+    {
+        cerr << "Размеры должны быть положительными.\n";
+        return -1;
+    }
+
+    ofstream out("pic.txt");
+    if (!out)
+    {
+        cerr << "Не удалось создать файл pic.txt\n";
+        return -1;
+    }
+
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            out << (rand() % 2);
+        }
+        out << endl;
+    }
+    out.close();
+
+    cout << "Бинарная картина сохранена в pic.txt" << endl;
     return 0;
 }
