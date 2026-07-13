ccc3fbc5d734c577ff0de55ac95ef42379884a62
Troyanok <83295437+Troyanok@users.noreply.github.com>
2026-07-13 13:47:53 +0300

    Update main2.cpp

--- task20/main2.cpp
+++ task20/main2.cpp
@@ -1,10 +1,64 @@
 #include <iostream>
 #include <string>
-#include <cctype>
+#include <map>
 
 using namespace std;
 
 int main()
 {
+    map<string, int> queue;
+    string input;
+    string command;
+
+    cout << "Добро пожаловать в регистратуру: " << endl; 
+    while (1)
+    {
+        cout << "Выберите команду для работы регистратуры: " << endl;
+        cout << "\t add: Добавить человека в очередь;" << endl;
+        cout << "\t Next: Вызвать человека в регистратуру по алфавитному порядку, который первый в очереди;" << endl;
+        cout << "\t quit: Завершить работу программы."<< endl;
+        cin >> command;
+        cin.ignore();
+
+        if (command == "quit")
+        {
+            break;
+        }
+
+        if (command == "Next")
+        {
+
+            if (!queue.empty())
+            {
+                auto it = queue.begin();
+                cout << it->first << endl;
+                if (--(it->second) == 0)
+                {
+                    queue.erase(it);
+                }
+            }
+            else 
+            {
+                cout << "Очередь пустая!" << endl;
+            }
+        }
+        else if (command == "add")
+        {
+            cout << "Введите строку: ";
+            getline(std::cin, input);
+
+            if (input.empty()) 
+            {
+                cout << "Введена пустая строка!" << endl;
+                continue;
+            }
+            queue[input] += 1;
+        }
+        else
+        {
+            cout << "Была введена неизвестная команда!" << endl;
+        }
+        cout << endl;
+    }
     return 0;
 }
