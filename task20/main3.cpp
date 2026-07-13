f1c1438b41bcd7c6535249b8f6eadf190e48bce7
Troyanok <83295437+Troyanok@users.noreply.github.com>
2026-07-13 13:57:06 +0300

    Update main3.cpp

--- task20/main3.cpp
+++ task20/main3.cpp
@@ -1,10 +1,49 @@
 #include <iostream>
 #include <string>
-#include <cctype>
+#include <map>
 
 using namespace std;
 
+bool CheckAnagrams(const string& s1, const string& s2)
+{
+    if (s1.length() != s2.length()) 
+    {
+        return false;
+    }
+
+    map<char, int> freq;
+    for (char c : s1)
+    {
+        ++freq[c];
+    }
+    for (char c : s2)
+    {
+        --freq[c];
+    }
+
+    for (const auto& pair : freq)
+    {
+        if (pair.second != 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main()
 {
+    string a;
+    string b;
+
+    cout << "Введите первую строку для проверки: ";
+    cin >> a;
+    cin.ignore();
+
+    cout << "Введите вторую строку для проверки: ";
+    cin >> b;
+    cin.ignore();
+
+    cout << "Проверка анаграммы: " << boolalpha << CheckAnagrams(a, b) << endl;
     return 0;
 }
