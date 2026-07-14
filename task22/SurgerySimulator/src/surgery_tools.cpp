f8d19305c4ee6058fde0118b9025d822bba1039f
Troyanok <83295437+Troyanok@users.noreply.github.com>
2026-07-14 13:40:29 +0300

    Update surgery_tools.cpp

--- task22/SurgerySimulator/src/surgery_tools.cpp
+++ task22/SurgerySimulator/src/surgery_tools.cpp
@@ -21,7 +21,7 @@ bool CheckEqualPoints(const Point& first, const Point& second)
     return (first.x == second.x) && (first.y == second.y);
 }
 
-void scalpel(const Point &first, const Point& second)
+void ScalpelAction(const Point &first, const Point& second)
 {
     std::cout << "Выполнен разрез между точками ";
     PrintPoint(first);
@@ -30,21 +30,21 @@ void scalpel(const Point &first, const Point& second)
     std::cout << std::endl;
 }
 
-void hemostat(const Point& currentPoint)
+void HemostatAction(const Point& currentPoint)
 {
     std::cout << "Зажим установлен в точке ";
     PrintPoint(currentPoint);
     std::cout << std::endl;
 }
 
-void tweezers(const Point& currentPoint)
+void TweezersAction(const Point& currentPoint)
 {
     std::cout << "Пинцет установлен в точке ";
     PrintPoint(currentPoint);
     std::cout << std::endl;
 }
 
-void suture(const Point& first, const Point& second)
+void SutureAction(const Point& first, const Point& second)
 {
     std::cout << "Установить шов между точками ";
     PrintPoint(first);
