b9a38747131eb21b798669304d68684f2e7d5369
Troyanok <83295437+Troyanok@users.noreply.github.com>
2026-07-14 14:14:41 +0300

    Create ram.cpp

---
+++ task22/WorkingPCSimulator/src/ram.cpp
@@ -0,0 +1,30 @@
+#include <include/constants.h>
+#include <include/ram.h>
+
+namespace ram_tools
+{
+namespace
+{
+
+int ram_memory[value_constants::MAX_RAM_VALUE] = {};
+
+}
+
+void WriteBuffer(int index, int value) noexcept
+{
+    if ((index >= value_constants::MIN_RAM_VALUE) && (index < value_constants::MAX_RAM_VALUE))
+    {
+        ram_memory[index] = value;
+    }
+}
+
+int ReadBuffer(int index) noexcept
+{
+    if ((index >= value_constants::MIN_RAM_VALUE) && (index < value_constants::MAX_RAM_VALUE))
+    {
+        return ram_memory[index];
+    }
+    return 0;
+}
+
+}
