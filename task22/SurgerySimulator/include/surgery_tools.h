53e8529f42849e681d02ecb42846fc2293bcda44
Troyanok <83295437+Troyanok@users.noreply.github.com>
2026-07-14 12:13:07 +0300

    Update surgery_tools.h

--- task22/SurgerySimulator/include/surgery_tools.h
+++ task22/SurgerySimulator/include/surgery_tools.h
@@ -1,10 +1,30 @@
 #include <iostream>
-#include <string>
 
-using namespace std;
+#pragma once
 
-
-int main()
+namespace surgery_tools
 {
-   return 0;
+
+struct Point
+{
+    double x;
+    double y;
+
+    Point();
+};
+
+void InputPoint(Point& curPoint);
+
+bool CheckEqualPoints(const Point& first, const Point& second);
+
+void scalpel(const Point& first, const Point& second);
+
+void hemostat(const Point& currentPoint);
+
+void tweezers(const Point& currentPoint);
+
+void suture(const Point& first, const Point& second);
+
+void PrintPoint(const Point& currentPoint);
+
 }
