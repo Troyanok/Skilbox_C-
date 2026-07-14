c765bcd3774453e04f2a8f42ea26945f17b3f70c
Troyanok <83295437+Troyanok@users.noreply.github.com>
2026-07-14 14:11:56 +0300

    Create constants.h

---
+++ task22/WorkingPCSimulator/include/constants.h
@@ -0,0 +1,10 @@
+namespace value_constants
+{
+
+/// @brief Максимальное значение буфера памяти
+constexpr int MAX_RAM_VALUE { 8 };
+
+/// @brief Минимальное значение буфера памяти
+constexpr int MIN_RAM_VALUE { 0 };
+
+}
