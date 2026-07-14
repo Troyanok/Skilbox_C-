5a9e8ffb7f6a8168f10550292f0c11d1ec3e6a0e
Troyanok <83295437+Troyanok@users.noreply.github.com>
2026-07-14 12:13:27 +0300

    Update surgery_tools.cpp

--- task22/SurgerySimulator/src/surgery_tools.cpp
+++ task22/SurgerySimulator/src/surgery_tools.cpp
@@ -1,1 +1,61 @@
+#include <include/surgery_tools.h>
 
+namespace surgery_tools
+{
+
+Point::Point()
+    : x(0)
+    , y(0)
+{};
+
+void InputPoint(Point& curPoint)
+{
+    std::cout << "Введите координату X: ";
+    std::cin >> curPoint.x;
+    std::cout << "Введите координату Y: ";
+    std::cin >> curPoint.y;
+}
+
+bool CheckEqualPoints(const Point& first, const Point& second)
+{
+    return (first.x == second.x) && (first.y == second.y);
+}
+
+void scalpel(const Point &first, const Point& second)
+{
+    std::cout << "Выполнен разрез между точками ";
+    PrintPoint(first);
+    std::cout << " и ";
+    PrintPoint(second);
+    std::cout << std::endl;
+}
+
+void hemostat(const Point& currentPoint)
+{
+    std::cout << "Зажим установлен в точке ";
+    PrintPoint(currentPoint);
+    std::cout << std::endl;
+}
+
+void tweezers(const Point& currentPoint)
+{
+    std::cout << "Пинцет установлен в точке ";
+    PrintPoint(currentPoint);
+    std::cout << std::endl;
+}
+
+void suture(const Point& first, const Point& second)
+{
+    std::cout << "Установить шов между точками ";
+    PrintPoint(first);
+    std::cout << " и ";
+    PrintPoint(second);
+    std::cout << std::endl;
+}
+
+void PrintPoint(const Point& currentPoint)
+{
+    std::cout << "(" << currentPoint.x << ", " << currentPoint.y << ")";
+}
+
+}
