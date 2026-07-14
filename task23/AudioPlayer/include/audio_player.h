17840d31ca8fdeedb6da080c84d4ceef757b6696
Troyanok <83295437+Troyanok@users.noreply.github.com>
2026-07-14 18:44:14 +0300

    Update audio_player.h

--- task23/AudioPlayer/include/audio_player.h
+++ task23/AudioPlayer/include/audio_player.h
@@ -1,10 +1,36 @@
 #include <iostream>
-#include <string>
+#include <include/track.h>
 
-using namespace std;
-
-
-int main()
+/// @brief Класс аудиоплеера
+class AudioPlayer
 {
-   return 0;
-}
+public:
+    /// @brief Конструктор по умолчанию
+    AudioPlayer();
+
+    /// @brief Конструктор
+    /// @param[in] fileName Имя файла для получения вектора треков
+    AudioPlayer(const std::string& fileName);
+
+    /// @brief Деструктор по умолчания
+    ~AudioPlayer() = default;
+
+    /// @brief Воспроизвести трек
+    /// @param[in] targetName Имя трека для воспроизведения
+    void PlayTrack(const std::string& targetName);
+
+    /// @brief Остановить воспроизведение трека
+    void StopTrack();
+
+    /// @brief Поставить на паузу воспроизведение трека
+    void PauseTrack();
+
+    /// @brief Переключиться на следующий трек
+    void NextTrack();
+
+private:
+    std::vector<Track> audioList_; ///< Вектор треков
+    bool isPlay_;                  ///< Флаг воспроизведения
+    bool isPause_;                 ///< Флаг паузы
+    Track currentTrack_;           ///< Текущий трек
+};
