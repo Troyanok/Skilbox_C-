9804fcba6adf4a9c681ab730dd8b9ebf944706f1
Troyanok <83295437+Troyanok@users.noreply.github.com>
2026-07-17 14:06:43 +0300

    Create contact.h

---
+++ task23/PhoneSimulator/include/contact.h
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+/// @brief Класс для хранения информации о контакте
+class Contact
+{
+public:
+    /// @brief Конструктор по умолчанию
+    Contact();
+
+    /// @brief Конструктор 
+    Contact(const std::string& name, const std::string& phone);
+
+    /// @brief Деструктор по умолчанию
+    ~Contact() = default;
+
+    /// @brief Получить имя контакта
+    std::string GetContactName() const;
+
+    /// @brief Получить номер контакта
+    std::string GetPhoneName() const;
+
+private:
+    std::string name_;  ///< номер контакта
+    std::string phone_; ///< номер телефона
+};
