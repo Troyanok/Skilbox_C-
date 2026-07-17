2fb155acd4238ffa40d67fe7d496a6b960fe4b51
Troyanok <83295437+Troyanok@users.noreply.github.com>
2026-07-17 17:22:39 +0300

    Update window.h

--- task23/WindowControl/include/window.h
+++ task23/WindowControl/include/window.h
@@ -1,1 +1,55 @@
+#pragma once
 
+#include <iostream>
+
+/// @brief Класс пользовательского окна
+class Window
+{
+public:
+    /// @brief Конструктор
+    Window();
+
+    /// @brief Конструктор с параметрами
+    /// @param x Начальная координата OX
+    /// @param y Начальная координата OY
+    /// @param width Высота окна
+    /// @param height Ширина окна
+    Window(int x, int y, int width, int height);
+
+    /// @brief Деструктор по умолчанию
+    ~Window() = default;
+
+    /// @brief Передвинуть окно пользователя
+    /// @param dx Смещение по оси OX
+    /// @param dy Смещение по оси OY
+    /// @return true - получилось переместить, иначе - false
+    bool MoveWindow(int dx, int dy);
+
+    /// @brief Изменить размер окна
+    /// @param newWidth Новая высота
+    /// @param newHeight Новая ширина
+    /// @return true - получилось изменить размер окна, иначе - false
+    bool ResizeWindow(int newWidth, int newHeight);
+
+    /// @brief Получить начальную координату OX
+    /// @return Начальная координата OX
+    int GetX() const;
+
+    /// @brief Получить начальную координату OY
+    /// @return Начальная координата OY
+    int GetY() const;
+
+    /// @brief Получить высоту окна
+    /// @return Высота окна
+    int GetWidth() const;
+
+    /// @brief Получить ширину окна
+    /// @return Ширина окна
+    int GetHeight() const;
+
+private:
+    int x_;         ///< начальная координата OX
+    int y_;         ///< начальная координата OY
+    int width_;     ///< Высота 
+    int height_;    ///< Ширина
+};
