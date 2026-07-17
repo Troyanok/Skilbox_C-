986c785f46432960185d50fa19091e3d53d62fb9
Troyanok <83295437+Troyanok@users.noreply.github.com>
2026-07-17 17:22:57 +0300

    Create constants.h

---
+++ task23/WindowControl/include/constants.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <iostream>
+
+/// @brief Область для размеров окна
+namespace window_size
+{
+
+static const int WindowWidth { 80 };
+    
+static const int WindowHeight { 50 };
+
+} // namespace window_size
