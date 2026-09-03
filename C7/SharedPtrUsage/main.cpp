f7032742beaad516e1525cb44df2fa83a88ae9b4
Troyanok <83295437+Troyanok@users.noreply.github.com>
2026-09-03 21:11:15 +0300

    Update main.cpp

--- C7/SharedPtrUsage/main.cpp
+++ C7/SharedPtrUsage/main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
-#include <string>
-
-using namespace std;
-
+#include <memory>
+#include <include/dog.h>
+#include <include/toy.h>
 
 int main()
 {
-   return 0;
+    auto ball = std::make_shared<Toy>("Ball");
+    auto bone = std::make_shared<Toy>("Bone");
+
+    Dog dog1("Rex");
+    Dog dog2("Buddy");
+
+    dog1.GetToy(ball);
+    dog2.GetToy(ball);
+    dog2.GetToy(bone);
+    dog1.GetToy(ball);
+    dog1.DropToy();
+    dog2.GetToy(ball);
+    dog2.DropToy();
+
+    return 0;
 }
