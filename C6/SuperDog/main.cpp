ad0dbfa01ca65b0fc3b96fa9c45880ea4a930457
Troyanok <83295437+Troyanok@users.noreply.github.com>
2026-09-03 20:40:37 +0300

    Update main.cpp

--- C6/SuperDog/main.cpp
+++ C6/SuperDog/main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
-#include <string>
-
-using namespace std;
-
+#include <memory>
+#include <include/dog.h>
+#include <include/swimming.h>
+#include <include/dancing.h>
+#include <include/counting.h>
 
 int main()
 {
-   return 0;
+    // Создаём собаку
+    Dog dog1("Steve");
+
+    // Добавляем таланты
+    dog1.AddTalent(std::make_unique<Dancing>());
+    dog1.AddTalent(std::make_unique<Swimming>());
+
+    // Показываем таланты
+    dog1.ShowTalents();
+
+    // Добавляем ещё один талант и показываем снова
+    dog1.AddTalent(std::make_unique<Counting>());
+    dog1.ShowTalents();
+
+    return 0;
 }
