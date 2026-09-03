7ca15887190b5b9b26e73bf5581f7051b36ccb85
Troyanok <83295437+Troyanok@users.noreply.github.com>
2026-09-03 21:17:57 +0300

    Update toy.h

--- C7/CustomSharedPtr/include/toy.h
+++ C7/CustomSharedPtr/include/toy.h
@@ -1,1 +1,32 @@
+#pragma once
 
+#include <string>
+#include <iostream>
+
+/// @brief Класс игрушки
+class Toy
+{
+public:
+    /// @brief Конструктор
+    /// @param name Имя игрушки
+    Toy(const std::string& name)
+    {
+        name_ = name;
+    }
+
+    /// @brief Деструктор Выводит сообщение об удалении
+    ~Toy()
+    {
+        std::cout << "Toy " << name_ << " was dropped" << std::endl;
+    }
+
+    /// @brief Возвращает имя игрушки
+    /// @return Имя
+    std::string GetName() const
+    {
+        return name_;
+    }
+
+private:
+    std::string name_; ///< Имя
+};
