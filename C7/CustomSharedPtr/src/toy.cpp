dfe6d31a9f35b61f7448c1cacb38fee624809ea3
Troyanok <83295437+Troyanok@users.noreply.github.com>
2026-09-03 21:20:41 +0300

    Add Toy class implementation in toy.cpp

---
+++ C7/CustomSharedPtr/src/toy.cpp
@@ -0,0 +1,14 @@
+#include <include/toy.h>
+
+Toy::Toy(const std::string& name) : name_(name)
+{}
+
+Toy::~Toy()
+{
+    std::cout << "Toy " << name_ << " was dropped" << std::endl;
+}
+
+std::string Toy::GetName() const
+{
+    return name_;
+}
