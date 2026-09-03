144978ac42d4583476f8fd94e3292878956eb270
Troyanok <83295437+Troyanok@users.noreply.github.com>
2026-09-03 20:20:12 +0300

    Create  swimming.h

---
+++ C6/SuperDog/include/swimming.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <include/talent.h>
+
+/// @brief Талант «Плавание»
+class Swimming : public Talent
+{
+public:
+    /// @brief Возвращает название таланта.
+    /// @return Строка "Swim"
+    std::string GetTalentName() const override
+    {
+        return "Swim";
+    }
+};
