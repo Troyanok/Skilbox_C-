702e715cb7dcd5d5f5ff7098fea1c710e33df990
Troyanok <83295437+Troyanok@users.noreply.github.com>
2026-09-08 14:13:05 +0300

    Add command-line interface for shopping cart
    Implement command-line interface for shopping cart operations.

--- C9/ShoppingCart/main.cpp
+++ C9/ShoppingCart/main.cpp
@@ -1,10 +1,57 @@
 #include <iostream>
 #include <string>
-
-using namespace std;
-
+#include "include/shopping_cart.h"
 
 int main()
 {
-   return 0;
+    ShoppingCart cart;
+    std::string command;
+    std::cout << "Commands: add_store, add, remove, print, exit" << std::endl;
+
+    while (true)
+    {
+        std::cout << "> ";
+        std::cin >> command;
+        try
+           {
+            if (command == "add_store")
+            {
+                std::string article;
+                int qty;
+                std::cin >> article >> qty;
+                cart.AddToStore(article, qty);
+            }
+            else if (command == "add")
+            {
+                std::string article;
+                int qty;
+                std::cin >> article >> qty;
+                cart.AddToCart(article, qty);
+            }
+            else if (command == "remove")
+            {
+                std::string article;
+                int qty;
+                std::cin >> article >> qty;
+                cart.RemoveFromCart(article, qty);
+            }
+            else if (command == "print")
+            {
+                cart.PrintCart();
+            }
+            else if (command == "exit")
+            {
+                break;
+            }
+            else
+            {
+                std::cout << "Unknown command" << std::endl;
+            }
+        }
+        catch (const std::exception& e)
+           {
+            std::cerr << "Error: " << e.what() << std::endl;
+        }
+    }
+    return 0;
 }
