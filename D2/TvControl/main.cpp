baf7a9a32bc7468e60a639d2ab43b35b71d6c8d9
Troyanok <83295437+Troyanok@users.noreply.github.com>
2026-09-09 14:14:29 +0300

    Refactor main function for Qt application setup
    Updated main function to use Qt framework for GUI application.

--- D2/TvControl/main.cpp
+++ D2/TvControl/main.cpp
@@ -1,10 +1,25 @@
-#include <iostream>
-#include <string>
-#include <cctype>
+#include <QtGui/QGuiApplication>
+#include <QtQml/QQmlApplicationEngine>
 
-using namespace std;
-
-int main()
+int main(int argc, char *argv[])
 {
-    return 0;
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreated,
+        &app,
+        [url](QObject *obj, const QUrl &objUrl) {
+            if (!obj && url == objUrl)
+                QCoreApplication::exit(-1);
+        },
+        Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
 }
